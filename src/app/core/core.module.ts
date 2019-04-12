7f913fd323f4f9e94a11238f2476cbe6745d5e10
Bruno Quinbal <brunorm46@gmail.com>
2019-04-12 09:28:38 +0200

    Minor changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from '../app-routing.module';
-import { HomeComponent } from '../home/home/home.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, MainComponent, FooterComponent, HomeComponent],
+  declarations: [LayoutComponent, HeaderComponent, MainComponent, FooterComponent],
   imports: [CommonModule, AppRoutingModule],
   exports: [LayoutComponent]
 })
