9db8e2ad1cc2bf751230d59ad186de5521f33478
Bruno Quinbal <brunorm46@gmail.com>
2019-04-15 12:18:57 +0200

    'pub'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, BrowserAnimationsModule],
+  imports: [BrowserModule, CoreModule, BrowserAnimationsModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
